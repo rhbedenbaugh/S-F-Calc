d462d95b78f168d074d5a560c73af6fe00a38722
Robert Bedenbaugh <95264763+rhbedenbaugh@users.noreply.github.com>
2023-01-26 20:02:25 -0500

    fixed imperial to metric conversion

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ function calculate() {
   );
   let workpieceMaterial =
     document.querySelector('#workpieceMaterial').value;
-  let idealSFM = parseInt(document.querySelector('#idealSFM').value);
+  let idealSurfaceSpeed = parseInt(document.querySelector('#idealSurfaceSpeed').value);
   let idealChipLoad = document.querySelector('#idealChipLoad').value;
 
   if (!toolDiameter || !cuttingEdges) {
-    message.innerHTML = 'Enter tool diameter & cutting edges.';
+    message1.innerHTML = 'Enter tool diameter & cutting edges.';
     return;
   }
 
   let multiplier;
-  if (units === 'inches') multiplier = 1;
+  if (units === 'inches') multiplier = 1.;
   if (units === 'millimeters') multiplier = 25.4;
   
   let toolMaterialReducer;
@@ -26,59 +26,66 @@ function calculate() {
   if (toolMaterial === 'carbide') toolMaterialReducer = 1;
   if (toolMaterial === 'coatedCarbide') toolMaterialReducer = 1.2;
 
-  let SFM;
-  if (idealSFM) {
-    SFM = idealSFM;
+  let surfaceSpeed;
+  if (idealSurfaceSpeed) {
+    surfaceSpeed = idealSurfaceSpeed;
   } else {
-    if (workpieceMaterial == 'aluminumAlloys') SFM = 600;
-    if (workpieceMaterial == 'brassSoftBronze') SFM = 400;
-    if (workpieceMaterial == 'bronzeHighTensile') SFM = 250;
-    if (workpieceMaterial == 'cooperAlloys') SFM = 350;
-    if (workpieceMaterial == 'ironCastSoft') SFM = 200;
-    if (workpieceMaterial == 'ironCastHard') SFM = 100;
-    if (workpieceMaterial == 'ironDuctile') SFM = 80;
-    if (workpieceMaterial == 'ironMalleable') SFM = 250;
-    if (workpieceMaterial == 'magnesiumAlloys') SFM = 800;
-    if (workpieceMaterial == 'molybdenum') SFM = 800;
-    if (workpieceMaterial == 'monelHighNickelSteel') SFM = 150;
-    if (workpieceMaterial == 'nickelBaseHighTempAlloys') SFM = 20;
-    if (workpieceMaterial == 'plastics') SFM = 600;
-    if (workpieceMaterial == 'plasticsGlassFilled') SFM = 300;
-    if (workpieceMaterial == 'refractoryAlloys') SFM = 80;
-    if (workpieceMaterial == 'steelLowCarbon') SFM = 250;
-    if (workpieceMaterial == 'steelMediumCarbon') SFM = 100;
-    if (workpieceMaterial == 'steelUpToRc35') SFM = 150;
-    if (workpieceMaterial == 'steelRc35ToRc50') SFM = 80;
-    if (workpieceMaterial == 'steelRc50-Rc60') SFM = 25;
-    if (workpieceMaterial == 'steelMold') SFM = 200;
-    if (workpieceMaterial == 'steelTool') SFM = 100;
-    if (workpieceMaterial == 'stainlessSteelSoft') SFM = 250;
-    if (workpieceMaterial == 'stainlessSteelHard') SFM = 50;
-    if (workpieceMaterial == 'titaniumSoft') SFM = 120;
-    if (workpieceMaterial == 'titaniumHard') SFM = 30;
+    if (workpieceMaterial == 'aluminumAlloys') surfaceSpeed = 600;
+    if (workpieceMaterial == 'brassSoftBronze') surfaceSpeed = 400;
+    if (workpieceMaterial == 'bronzeHighTensile') surfaceSpeed = 250;
+    if (workpieceMaterial == 'cooperAlloys') surfaceSpeed = 350;
+    if (workpieceMaterial == 'ironCastSoft') surfaceSpeed = 200;
+    if (workpieceMaterial == 'ironCastHard') surfaceSpeed = 100;
+    if (workpieceMaterial == 'ironDuctile') surfaceSpeed = 80;
+    if (workpieceMaterial == 'ironMalleable') surfaceSpeed = 250;
+    if (workpieceMaterial == 'magnesiumAlloys') surfaceSpeed = 800;
+    if (workpieceMaterial == 'molybdenum') surfaceSpeed = 800;
+    if (workpieceMaterial == 'monelHighNickelSteel') surfaceSpeed = 150;
+    if (workpieceMaterial == 'nickelBaseHighTempAlloys') surfaceSpeed = 20;
+    if (workpieceMaterial == 'plastics') surfaceSpeed = 600;
+    if (workpieceMaterial == 'plasticsGlassFilled') surfaceSpeed = 300;
+    if (workpieceMaterial == 'refractoryAlloys') surfaceSpeed = 80;
+    if (workpieceMaterial == 'steelLowCarbon') surfaceSpeed = 250;
+    if (workpieceMaterial == 'steelMediumCarbon') surfaceSpeed = 100;
+    if (workpieceMaterial == 'steelUpToRc35') surfaceSpeed = 150;
+    if (workpieceMaterial == 'steelRc35ToRc50') surfaceSpeed = 80;
+    if (workpieceMaterial == 'steelRc50-Rc60') surfaceSpeed = 25;
+    if (workpieceMaterial == 'steelMold') surfaceSpeed = 200;
+    if (workpieceMaterial == 'steelTool') surfaceSpeed = 100;
+    if (workpieceMaterial == 'stainlessSteelSoft') surfaceSpeed = 250;
+    if (workpieceMaterial == 'stainlessSteelHard') surfaceSpeed = 50;
+    if (workpieceMaterial == 'titaniumSoft') surfaceSpeed = 120;
+    if (workpieceMaterial == 'titaniumHard') surfaceSpeed = 30;
   }
+
+  surfaceSpeed = (surfaceSpeed * 12 * multiplier)
+
   let FPT;
   if (idealChipLoad) {
     FPT = idealChipLoad * cuttingEdges;
   } else {
-    if (toolDiameter < 0.125) FPT = 0.0005;
-    if (toolDiameter < 0.25) FPT = 0.001;
-    if (toolDiameter < 0.4) FPT = 0.002;
-    if (toolDiameter < 0.6) FPT = 0.003;
-    if (toolDiameter > 0.6) FPT = 0.005;
+    if ((toolDiameter / multiplier) < 0.125) FPT = 0.0005;
+    if ((toolDiameter / multiplier) < 0.25) FPT = 0.001;
+    if ((toolDiameter / multiplier) < 0.4) FPT = 0.002;
+    if ((toolDiameter / multiplier) < 0.6) FPT = 0.003;
+    if ((toolDiameter / multiplier) > 0.6) FPT = 0.005;
   }
 
-  //calculate
+  FPT = (FPT * multiplier)
+
+  //calculations
   let speed = parseInt(
-    (SFM * 12 * multiplier * toolMaterialReducer) / (toolDiameter * Math.PI)
+    (surfaceSpeed ) / (toolDiameter * Math.PI)
   );
-  let feed = parseInt(speed * FPT * cuttingEdges * multiplier * toolMaterialReducer);
+  let feed = parseInt(speed * FPT * cuttingEdges * toolMaterialReducer);
 
   if (multiplier === 1) {
-    message.innerHTML = feed + '"/min at ' + speed + ' RPM.';
+    message1.innerHTML = feed + '"/min at ' + speed + ' RPM.';
+    message2.innerHTML = FPT + '" chip load at ' + (surfaceSpeed/12) + ' SFM';
     return;
   } else {
-    message.innerHTML = feed + 'mm/min at ' + speed + ' RPM.';
+    message1.innerHTML = feed + 'mm/min at ' + speed + ' RPM.';
+    message2.innerHTML = FPT + 'mm chip load at ' + parseInt(surfaceSpeed/1000) + ' SMM';
     return;
   }
 }
